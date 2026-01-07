2e3b072d1151ec34be44c529afa2ddfc4c27e2d4
Manmeet Singh <manmeetbadhan3104@gmail.com>
2026-01-07 12:53:28 +0530

    Solved X problems

--- 02 Patterns/21_pattern.cpp
+++ 02 Patterns/21_pattern.cpp
@@ -1,15 +1,26 @@
+/*
+
+4 4 4 4 4 4 4
+4 3 3 3 3 3 3
+4 3 2 2 2 2 3
+4 3 2 1 1 2 3 
+4 3 2 1 1 2 3
+4 3 2 2 2 2 3
+4 3 3 3 3 3 3 
+
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
 void pattern(int n) {
-    for (int i = 0; i < n; i++) {          // rows
-        for (int j = 0; j < n; j++) {      // columns
+    for (int i = 0; i < 2*n-1; i++) {          // rows
+        for (int j = 0; j < 2*n-1; j++) {      // columns
             int top = i;
             int left = j;
-            int right = n - 1 - j;
-            int bottom = n - 1 - i;
-            int layer = min(min(top, bottom), min(left, right)); 
-            cout << n - layer;
+            int right = (2*n - 1) - j;
+            int bottom = (2*n - 1) - i;
+            cout << n - min( min(top, bottom), min(left, right) );      //value at (i,j)
         }
         cout << endl;
     }
@@ -33,14 +44,33 @@ int main() {
 }
 
 
+
 /*
 
-7777777
-7666667
-7655567
-7654567
-7655567
-7666667
-7777777
+Example DRY RUN for n = 4:
+
+Cell (0,0): 
+i=0, j=0
+top=0, left=0, right=6, bottom=6 → min=0 → 4-0 = 4
+
+Cell (1,1):
+i=1, j=1
+top=1, left=1, right=5, bottom=5 → min=1 → 4-1 = 3
+
+Cell (2,2):
+i=2, j=2
+top=2, left=2, right=4, bottom=4 → min=2 → 4-2 = 2
+
+Cell (3,3):
+i=3, j=3
+top=3, left=3, right=3, bottom=3 → min=3 → 4-3 = 1
+
+Cell (3,4):
+i=3, j=4
+top=3, left=4, right=2, bottom=3 → min=2 → 4-2 = 2
+
+Cell (0,6):
+i=0, j=6
+top=0, left=6, right=0, bottom=6 → min=0 → 4-0 = 4
 
 */
--- 02 Patterns/22_pattern.cpp
+++ 02 Patterns/22_pattern.cpp
@@ -2,6 +2,31 @@
 using namespace std;
 
 void pattern(int n){
+    int space= 2*n-2;
+    for(int i =1;i<=2*n-1;i++){
+        
+        int stars=i; 
+        if(i>n) stars= 2*n-i;
+
+        //stars
+        for(int j=1;j<=stars;j++){
+            cout<<"*";
+        }
+
+        //space
+        for(int j=1;j<=space;j++){
+            cout<<" ";
+        }
+
+        //stars
+        for(int j=1;j<=stars;j++){
+            cout<<"*";
+        }
+
+        cout<<endl;
+        if(i<n) space-=2;
+        else space+=2;
+    }
      
 }
 
@@ -21,3 +46,17 @@ int main(){
     
     return 0;
 }
+
+/*
+
+*        *
+**      **
+***    ***
+****  ****
+**********
+****  ****
+***    ***
+**      **
+*        *
+
+*/
